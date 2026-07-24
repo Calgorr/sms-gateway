9d29660dbc13fcc80dcf4efed85fad5c7270dfa1
Amirali Homayouni <amirali.homayouni@snapp.cab>
2026-07-24 11:32:46 +0330

    Feat:implemented postgres init db

---
+++ pkg/postgres/client.go
@@ -0,0 +1,29 @@
+package postgres
+
+import (
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+
+	"github.com/calgorr/sms-gateway/config"
+)
+
+func NewDB(cfg config.Postgres) (*sqlx.DB, error) {
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host,
+		cfg.Port,
+		cfg.Username,
+		cfg.Password,
+		cfg.Database,
+	)
+
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("connect to postgres: %w", err)
+	}
+
+	return db, nil
+}
